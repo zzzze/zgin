7f92562b586bb1e12d727c977682128d2ad81abf
zeshuan <zhangzs000@hotmail.com>
2021-12-17 21:05:47 +0800

    support route group

--- main.go
+++ main.go
@@ -7,22 +7,31 @@ import (
 
 func main() {
 	r := zgin.New()
-	r.GET("/", func(c *zgin.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello World</h1>")
+	r.GET("/index", func(c *zgin.Context) {
+		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
 	})
-	r.GET("/hello", func(c *zgin.Context) {
-		// expect /hello?name=zhangsan
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-	})
-	r.GET("/hello/:name", func(c *zgin.Context) {
-		// expect /hello/zhangsan
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Params["name"], c.Path)
-	})
-	r.POST("/login", func(c *zgin.Context) {
-		c.JSON(http.StatusOK, zgin.H{
-			"username": c.PostForm("username"),
-			"password": c.PostForm("password"),
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *zgin.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello World</h1>")
 		})
-	})
+		v1.GET("/hello", func(c *zgin.Context) {
+			// expect /hello?name=zhangsan
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *zgin.Context) {
+			// expect /hello/zhangsan
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Params["name"], c.Path)
+		})
+		v2.POST("/login", func(c *zgin.Context) {
+			c.JSON(http.StatusOK, zgin.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
+	}
 	r.Run(":9999")
 }
--- zgin/zgin.go
+++ zgin/zgin.go
@@ -6,33 +6,55 @@ import (
 
 type HandlerFunc func(c *Context)
 
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandlerFunc
+	engine      *Engine
+}
+
 type Engine struct {
-  router *router
+	*RouterGroup
+	router *router
+	groups []*RouterGroup
 }
 
 func New() *Engine {
-  return &Engine{
-    router: newRouter(),
+  engine := &Engine{
+		router: newRouter(),
+	}
+  engine.RouterGroup = &RouterGroup{
+    engine: engine,
   }
+  engine.groups = []*RouterGroup{engine.RouterGroup}
+  return engine
 }
 
-func (engine *Engine) addRoute(method, pattern string, handler HandlerFunc) {
-  engine.router.addRoute(method, pattern, handler)
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+  newGroup := &RouterGroup{
+    prefix: group.prefix + prefix,
+    engine: group.engine,
+  }
+  group.engine.groups = append(group.engine.groups, newGroup)
+  return newGroup
 }
 
-func (engine *Engine) GET(pattern string, handler HandlerFunc) {
-  engine.addRoute(http.MethodGet, pattern, handler)
+func (group *RouterGroup) addRoute(method, pattern string, handler HandlerFunc) {
+	group.engine.router.addRoute(method, group.prefix + pattern, handler)
 }
 
-func (engine *Engine) POST(pattern string, handler HandlerFunc) {
-  engine.addRoute(http.MethodPost, pattern, handler)
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRoute(http.MethodGet, pattern, handler)
+}
+
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRoute(http.MethodPost, pattern, handler)
 }
 
 func (engine *Engine) Run(addr string) error {
-  return http.ListenAndServe(addr, engine)
+	return http.ListenAndServe(addr, engine)
 }
 
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-  c := newContext(w, req)
-  engine.router.handle(c)
+	c := newContext(w, req)
+	engine.router.handle(c)
 }
